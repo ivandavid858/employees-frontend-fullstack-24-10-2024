d3a85da335c4651fd8e8d76e1c14f62588444b84
ivandavid858 <ivandavid858@gmail.com>
2024-10-24 18:38:45 +0200

    Update environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    backend: 'https://employees-backend-fullstack-23-10-2024-16.onrender.com',
-    backendWs: 'wss://employees-backend-fullstack-23-10-2024-16.onrender.com'
+    backend: 'https://employees-backend-fullstack-23-10-2024-16.onrender.com/api/v1/employees',
+    backendWs: 'wss://employees-backend-fullstack-23-10-2024-16.onrender.com/api/v1/employees'
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-    production: true,
-    backend: 'http://localhost:8080/api/v1/employees',
-    backendWs: 'ws://employees-backend-fullstack-23-10-2024-16.onrender.com'
+    production: false,
+    backend: 'http://localhost:8080/api/v1/employees'
 }
