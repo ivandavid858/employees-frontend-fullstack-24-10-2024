38d6b09d0dfc16f040600a8d8b9762c8295ae752
ivandavid858 <ivandavid858@gmail.com>
2024-10-26 12:22:46 +0200

    Update environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
-/*export const environment = {
+export const environment = {
     production: true,
     //backend: 'https://employees-backend-fullstack-23-10-2024-16.onrender.com/api/v1/employees'
     backend: 'https://employees-backend-fullstack-23-10-2024-16.onrender.com/api/v1/'
-}*/
-export const environment = {
+}
+/*export const environment = {
     production: true,
-    backend: process.env["ANGULAR_APP_API_URL"] || 'https://employees-backend-fullstack-23-10-2024-16.onrender.com/api/v1/'
-};
+    backend: process.env["ANGULAR_APP_API_URL"] || 'https://employees-backend-fullstack-23-10-2024-16.onrender.com/api/v1/employees'
+};*/
